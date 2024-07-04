59aae8119ede47c40ab4c16a6c329d81c01cf351
covicale <ag.covic@gmail.com>
2024-07-04 16:47:57 +0200

    chore: update icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,8 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "ChromeGPT",
-  description: "ChatGPT Clone with Chrome Gemini's AI Integration",
-};
 
 export default function RootLayout({
   children,
@@ -16,6 +11,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>ChromeGPT</title>
+        <meta
+          name="description"
+          content="ChatGPT Clone with Chrome Gemini's AI Integration"
+        />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+      </Head>
       <body
         className={`min-h-screen bg-background font-sans antialiased ${inter.className}`}
       >
